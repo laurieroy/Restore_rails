a21697d47301236869f50066df6ba9220e456dd7
lauretteroy <laurieroy.dev@gmail.com>
2021-11-11 15:02:14 -0600

    Add Product model, minimum tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+	validates :name, presence: true, length: { minimum: 3 }
+	validates :price, presence: true
+	validates_numericality_of :price, greater_than_or_equal_to: 0
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_length_of(:name).is_at_least(3) }
+
+  it { should validate_presence_of(:price) }
+  it { should validate_numericality_of(:price) }
 end
